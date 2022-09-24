3b7d0a8f95bd8633673d6e81742ce664cf7759c7
shiodai <spgiots@gmail.com>
2022-09-25 07:49:53 +0900

    コミット

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\Stock;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,8 +22,10 @@ class DatabaseSeeder extends Seeder
             ImageSeeder::class,
             CategorySeeder::class,
             // ProductSeeder::class,
-            // StockSeeder::class
+            // StockSeeder::class,
+            UserSeeder::class,
         ]);
-        // \App\Models\User::factory(10)->create();
+        Product::factory(100)->create();
+        Stock::factory(100)->create();
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,11 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table("users")->insert([
+            "name" => "test",
+            "email" => "test@test.com",
+            "password" => Hash::make("password123"),
+            "created_at" => "2022/01/01 11:11:11"
+        ]);
     }
 }
